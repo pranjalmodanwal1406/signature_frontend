ef5a4559639512f43201d50d9b83439e45eecff3
pranjalmodanwal1406 <165895684+pranjalmodanwal1406@users.noreply.github.com>
2024-11-28 12:37:09 +0530

    IP change

--- src/component/ForgetPassword_newPassword/ForgetPassword_newPasssword.jsx
+++ src/component/ForgetPassword_newPassword/ForgetPassword_newPasssword.jsx
@@ -33,7 +33,7 @@ function ForgetPassword_newPassword() {
                 return;
             }
 
-            const response = await axios.post('http://44.196.192.232:9006/api/auth/resetPassword', {
+            const response = await axios.post('http://44.196.64.110:9006/api/auth/resetPassword', {
                 token,
                 newPassword: password
             });
